2df8b95b407d2d4abfec4cced87f30573caf9071
Raven <hong18s@gmail.com>
2012-09-11 11:40:10 +0900

    메모리 맵 출력시, 포인터를 사용했던 것을 어셈블리 함수인 MD로 교체했다. 교체된 부분은 아래와 같다. * 메모리 값 출력을 어셈블리 함수 MD의 return 값을 사용한다. * ASCII 출력부분 역시 MD 함수를 사용한다.

--- hex_viewer.c
+++ hex_viewer.c
@@ -16,18 +16,19 @@ int hex_viewer(int *address)
 
     FILE *fpout; // 파일 포인터 변수 선언.
 
-    unsigned char *c_num_ptr = (unsigned char *) address;
+    unsigned char *c_num_ptr = (unsigned char *)address;
 
     fpout = fopen("memory_status.txt", "w"); // fpout 변수에 텍스트파일을 쓰기가 가능하게 불러온다.
 
     // 가이드 문구 시작.
-    printf("---------------------------------------------------------------------------------\n"
+    printf("--------------------------------------------------------------------------------\n"
            " Address ");
 
     // 주소 마지막 바이트 출력 시작 : 불러들인 주소의 마지막 한 바이트를 알아보기 쉽게 띄워준다.
     for(i_cnt = 0;i_cnt <= 15;++i_cnt)
     {
-        printf("%02X ", ((unsigned char)c_num_ptr + i_cnt));
+        printf("%02X ", ((unsigned char)c_num_ptr + i_cnt - 16));
+        /* printf("%02X ", ((unsigned char)MD(address) + i_cnt)); */
         //fprintf(fpout, "%02X ",((unsigned char)c_num_ptr + i_cnt));
     }
     // 주소 마지막 바이트 출력 끝.
@@ -43,7 +44,8 @@ int hex_viewer(int *address)
         // 1바이트씩 주소 값을 출력 시작. :
         for(i_cnt = 0;i_cnt <= 15;++i_cnt)
         {
-            printf("%02x ", *(c_num_ptr + i_cnt));
+            /* printf("%02x ", *(c_num_ptr + i_cnt)); */
+            printf("%02x ", MD(address + i_cnt));
             fprintf(fpout, "%02x ", *(c_num_ptr + i_cnt)); // 텍스트 파일 출력.
         }
         // 1바이트씩 주소 값을 출력 끝. :
@@ -51,9 +53,10 @@ int hex_viewer(int *address)
         // 값에 대한 아스키 코드 출력 시작.
         for(i_cnt = 0;i_cnt <= 15;++i_cnt)
         {
-            if(' ' < *(((signed char *)c_num_ptr) + i_cnt)) // 제어문자 스페이스의 십진수 값은 32.
+            /* if(' ' < *(((signed char *)c_num_ptr) + i_cnt)) // 제어문자 스페이스의 십진수 값은 32. */
+            if(' ' < MD(address + i_cnt)) // 제어문자 스페이스의 십진수 값은 32.
             { // 이 값보다 출력 값이 크면 해당 아스키를 뿌리는데 이때
-                printf("%c", *(c_num_ptr + i_cnt)); // 제대로 표현할 확장 아스키를 걸러주기 위해 signed char 캐스팅.
+                printf("%c", MD(address + i_cnt)); // 제대로 표현할 확장 아스키를 걸러주기 위해 signed char 캐스팅.
                 fprintf(fpout, "%c", *(c_num_ptr + i_cnt)); // 텍스트 파일 출력.
             }
             else
@@ -64,6 +67,8 @@ int hex_viewer(int *address)
         }
         printf("\n"); // 한줄이 끝나고 개행(줄바꿈)
         fprintf(fpout, "\n"); // 텍스트 파일 출력.
+        
+        address = address + 16; // 다음 줄의 올바른 주소값은 위에서 16바이트를 출력했으니 다음줄은 기준이 되고 있는 첫바이트> 보다 16이 많다.
         c_num_ptr = c_num_ptr + 16; // 다음 줄의 올바른 주소값은 위에서 16바이트를 출력했으니 다음줄은 기준이 되고 있는 첫바이트> 보다 16이 많다.
     }
     fclose(fpout); // 파일을 닫아준다.
