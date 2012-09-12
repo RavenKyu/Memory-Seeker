6f9b2f12abcafa91aa83051e139faffe1ba1d1e0
Raven <hong18s@gmail.com>
2012-09-12 09:49:05 +0900

    compile 을 위한 batch 파일. 파일의 존재 유무를 확인한 후 전부 확인이 되었을 때만 링크를 진행하도록 작성되었다.

---
+++ first.bat
@@ -0,0 +1,27 @@
+﻿@echo off
+setlocal
+
+set main=main.obj
+set mornitor=mornitor.obj
+set hex=hex_viewer.obj
+
+del *.obj
+
+ml /c /coff mornitor.asm
+cl /c hex_viewer.c
+cl /c main.c
+
+IF EXIST %hex% (
+IF EXIST %main% (
+        IF EXIST %mornitor% (
+                cl %main% %mornitor% %hex%
+                echo %main%.exe :: 컴파일 완료.
+        ) ELSE (
+                echo %mornitor% 파일이 존재하지 않습니다.
+        )
+) ELSE (
+        echo %main% 파일이 존재하지 않습니다.
+)
+) ELSE (
+        echo %hex% 파일이 존재하지 않습니다.
+)
