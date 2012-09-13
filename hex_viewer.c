eac5bd9e552ef66a0e204b3d133194c04c8314ad
Raven <hong18s@gmail.com>
2012-09-14 01:32:51 +0900

    출력화면을 미약하게 개선 시켰다. * ASCII CODE 출력 부분에서 대응 주소를 나오게 끔 해두었지만 이 부분은 한번 더 개선의 필요성이 보인다.

--- hex_viewer.c
+++ hex_viewer.c
@@ -5,13 +5,13 @@
 //
 // 주석을 명료하며 되도록이면 다른 사람이 보고 이해하기 쉽도록 적었다.
 // 2012년 5월 11일.
+// 2012년 9월 14일 출력화면 개선
 #include <stdio.h>
 #include "hex_viewer.h"
 
 int hex_viewer(unsigned char *address)
 {
     int i_loop;
-    /* int i_num = 0xABCDEF88; */
     int i_cnt;
 
     FILE *fpout; // 파일 포인터 변수 선언.
@@ -21,7 +21,7 @@ int hex_viewer(unsigned char *address)
     fpout = fopen("memory_status.txt", "w"); // fpout 변수에 텍스트파일을 쓰기가 가능하게 불러온다.
 
     // 가이드 문구 시작.
-    printf("--------------------------------------------------------------------------------\n"
+    printf("--------------------------------------------------------------------------------"
            " Address ");
 
     // 주소 마지막 바이트 출력 시작 : 불러들인 주소의 마지막 한 바이트를 알아보기 쉽게 띄워준다.
@@ -29,15 +29,14 @@ int hex_viewer(unsigned char *address)
     for(i_cnt = 0;i_cnt <= 15;++i_cnt)
     {
         printf("%02X ", ((unsigned char)c_num_ptr + i_cnt));
-        /* printf("%02X ", ((unsigned char)MD(address) + i_cnt)); */
         //fprintf(fpout, "%02X ",((unsigned char)c_num_ptr + i_cnt));
     }
     // 주소 마지막 바이트 출력 끝.
 
-    printf(" ASCII CODE \n");
-    //fprintf(fpout, "\n", i_num);
+    printf("0123456789ABCDEF\n"); /* ASCII CODE가 출력되는 곳의 주소가이드 */
+
     // 몸체 시작. : 주소와 해당 주소의 실제값을 1 바이트 단위로 출력, 아스키 코드 표시.
-    for(i_loop = 0;i_loop <= 10;++i_loop) // 몇 줄의 주소를 띄울 것인가를 i_loop의 비교값으로 결정.
+    for(i_loop = 0; i_loop <= 15; ++i_loop) // 몇 줄의 주소를 띄울 것인가를 i_loop의 비교값으로 결정.
     {
         printf("0x%08x ", c_num_ptr); // 주소 출력
         fprintf(fpout, "%08x ", c_num_ptr); // 텍스트 파일 출력.
@@ -45,7 +44,6 @@ int hex_viewer(unsigned char *address)
         // 1바이트씩 주소 값을 출력 시작. :
         for(i_cnt = 0;i_cnt <= 15;++i_cnt)
         {
-            /* printf("%02x ", *(c_num_ptr + i_cnt)); */
             printf("%02x ", MD(address + i_cnt));
             fprintf(fpout, "%02x ", *(c_num_ptr + i_cnt)); // 텍스트 파일 출력.
         }
@@ -54,7 +52,6 @@ int hex_viewer(unsigned char *address)
         // 값에 대한 아스키 코드 출력 시작.
         for(i_cnt = 0;i_cnt <= 15;++i_cnt)
         {
-            /* if(' ' < *(((signed char *)c_num_ptr) + i_cnt)) // 제어문자 스페이스의 십진수 값은 32. */
             if(' ' < MD(address + i_cnt)) // 제어문자 스페이스의 십진수 값은 32.
             { // 이 값보다 출력 값이 크면 해당 아스키를 뿌리는데 이때
                 printf("%c", MD(address + i_cnt)); // 제대로 표현할 확장 아스키를 걸러주기 위해 signed char 캐스팅.
