312e8f9d5fd2891450c19a52c9774f20782875e4
Raven <hong18s@gmail.com>
2012-09-18 01:51:20 +0900

    줄 맞춤 IF 문이 제대로 동작하지 않아서 줄을 다시 맞추었다.

--- first.bat
+++ first.bat
@@ -13,15 +13,15 @@ cl /c main.c
 
 IF EXIST %hex% (
 IF EXIST %main% (
-        IF EXIST %mornitor% (
-                cl %main% %mornitor% %hex%
-                echo %main%.exe :: 컴파일 완료.
-        ) ELSE (
-                echo %mornitor% 파일이 존재하지 않습니다.
-        )
+IF EXIST %mornitor% (
+cl %main% %mornitor% %hex%
+echo %main%.exe :: 컴파일 완료.
 ) ELSE (
-        echo %main% 파일이 존재하지 않습니다.
+echo %mornitor% 파일이 존재하지 않습니다.
 )
 ) ELSE (
-        echo %hex% 파일이 존재하지 않습니다.
+echo %main% 파일이 존재하지 않습니다.
+)
+) ELSE (
+echo %hex% 파일이 존재하지 않습니다.
 )
